b34dc4afad990a70e41d1414314d945fe4a584fa
eishikachauhan <chauhaneishika@gmail.com>
2026-07-17 22:57:25 +0530

    Visible Person At Queue..

---
+++ SeePersonAtQueue.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+public class SeePersonAtQueue {
+    public int[] canSeePersonsCount(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[n-1]);
+        ans[n-1]=0;
+        for(int i=n-2; i>=0; i--) {
+            int count = 0;
+            while(st.size()>0 && st.peek()<=arr[i]) {
+                count++;
+                st.pop();
+            }
+            if(st.size()>0) count++; //very very imp
+            ans[i] = count;
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+}
